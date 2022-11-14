c70f75aeea3ef92a89c8d8b1919f10c73aa97331
Italovini223 <italovinicius2018@gmail.com>
2022-11-14 19:12:29 -0300

    ✨ feat: check if the email already exists

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,16 +1,31 @@
 const knex = require('../database/knex');
 const appError = require('../utils/appError');
+const {hash} = require('bcrypt');
 
 class UserController {
   async create(request, response) {
     const {name, email, password, isAdmin = false} = request.body
+
+    const checkUserExists = await knex("users").where({email})
+
+    console.log(checkUserExists)
+
+    if(checkUserExists.length > 0) {
+      throw new appError("Este email já existe, favor cadastrar outro!", 401)
+    }
+
+    const hashedPassword = await hash(password, 8);
     
     await knex("users").insert({
       name,
       email,
-      password,
+      password: hashedPassword,
       isAdmin
     });
+
+    return response.status(201).json({
+      message: "Usuário cadastrado com sucesso!",
+    })
   }
 }
 
