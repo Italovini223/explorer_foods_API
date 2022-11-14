06d887e9b9f8735e9cf749f2d91ed137d9aba3de
Italovini223 <italovinicius2018@gmail.com>
2022-11-14 12:19:07 -0300

    ✨ feat: add error handling in app

--- src/server.js
+++ src/server.js
@@ -1,6 +1,27 @@
+require('express-async-errors');
 const express = require('express');
 
+const AppError = require('./utils/appError')
+
 const app = express();
+app.use(express.json());
+
+app.use((error, request, response, next) => {
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: "bead request",
+      message: error.message
+    })
+  }
+
+  console.log(error.message)
+
+  return response.status(500).json({
+    status: "server error",
+    message: "internal server error"
+
+  });
+})
 
 const PORT = 3333;
 
