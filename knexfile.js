e7fc19733f61f4c6e3593f800b00c30d3575680c
Italovini223 <italovinicius2018@gmail.com>
2022-11-14 14:07:57 -0300

    ✨ feat: create table users in database

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,11 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+
     useNullAsDefault: true,
   },
 }
