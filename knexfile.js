cb09ed4f18b30624ea7c7e4ceddc4194dd5e1658
Italovini223 <italovinicius2018@gmail.com>
2022-11-14 13:47:21 -0300

    ✨ feat: add and config knex

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    useNullAsDefault: true,
+  },
+}
+ 
