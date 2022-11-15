11044989b7f4985f7c7a39b28d635cb88650eca3
Italovini223 <italovinicius2018@gmail.com>
2022-11-14 22:26:25 -0300

    ✨ feat: add column category in dish

--- src/database/knex/migrations/20221114223912_createDishs.js
+++ src/database/knex/migrations/20221114223912_createDishs.js
@@ -4,6 +4,7 @@ exports.up = knex => knex.schema.createTable("dish", table =>{
   table.increments("id");
   table.text("name");
   table.text("description");
+  table.text("category");
   table.integer("price");
   table.text("avatar");
 });
