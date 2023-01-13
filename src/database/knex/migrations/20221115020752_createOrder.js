64ee17711f8ea3965c52165d5f70a521a41c4f92
Italovini223 <italovinicius2018@gmail.com>
2023-01-13 15:08:14 -0300

    ✨ feat: create order and orderDishes table in db

--- src/database/knex/migrations/20221115020752_createOrder.js
+++ src/database/knex/migrations/20221115020752_createOrder.js
@@ -2,8 +2,12 @@
 exports.up = knex => knex.schema.createTable("orders", table => {
   table.increments("id");
   table.integer("user_id").references("id").inTable("users");
-  table.integer("total");
-  table.text("dishes");
+  
+  table.text("status");
+  table.text("total");
+  table.text("payMethod");
+
+  table.timestamp("crated_at").default(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable("orders");
